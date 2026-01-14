d1bd1fbf5c043a2b54f38953c9b5d415cbc4e7e0
lailabennani <laila.lahkim.bennani03@gmail.com>
2026-01-14 19:13:56 +0100

    basically did nothing :p

---
+++ src/murb/implem/SimulationNBodySIMD.hpp
@@ -0,0 +1,27 @@
+#ifndef SIMULATION_N_BODY_SIMD_HPP_
+#define SIMULATION_N_BODY_SIMD_HPP_
+
+#include <string>
+
+#include "core/SimulationNBodyInterface.hpp"
+
+class SimulationNBodySIMD : public SimulationNBodyInterface {
+  protected:
+    std::vector<accAoS_t<float>> accelerations; /*!< Array of body acceleration structures. */
+    
+    //MIPP modifications
+    const int floatN = mipp::N<float>();
+    accSoA_t<float> vAccelerations; /*!< Structure of arrays of body acceleration. */
+
+  public:
+    SimulationNBodySIMD(const unsigned long nBodies, const std::string &scheme = "galaxy", const float soft = 0.035f,
+                         const unsigned long randInit = 0);
+    virtual ~SimulationNBodySIMD() = default;
+    virtual void computeOneIteration();
+
+  protected:
+    void initIteration();
+    void computeBodiesAcceleration();
+};
+
+#endif /* SIMULATION_N_BODY_SIMD_HPP_ */
