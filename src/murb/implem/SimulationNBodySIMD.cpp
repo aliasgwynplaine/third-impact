1750c914f32c6cf19ad4b6db4b0a7d6d05c3fb2b
lailabennani <laila.lahkim.bennani03@gmail.com>
2026-01-19 16:46:51 +0100

    still doesnt work but at least it makes sense now

--- src/murb/implem/SimulationNBodySIMD.cpp
+++ src/murb/implem/SimulationNBodySIMD.cpp
@@ -1,4 +1,4 @@
-include <mipp.h>
+#include <mipp.h>
 
 #include <cassert>
 #include <cmath>
@@ -9,79 +9,86 @@ include <mipp.h>
 
 #include "SimulationNBodySIMD.hpp"
 
-SimulationNBodyNaive::SimulationNBodyNaive(const unsigned long nBodies, const std::string &scheme, const float soft,
+SimulationNBodySIMD::SimulationNBodySIMD(const unsigned long nBodies, const std::string &scheme, const float soft,
                                            const unsigned long randInit)
     : SimulationNBodyInterface(nBodies, scheme, soft, randInit)
 {
     this->flopsPerIte = 20.f * (float)this->getBodies().getN() * (float)this->getBodies().getN();
-    this->accelerations.resize(this->getBodies().getN());
 }
-
-void SimulationNBodyNaive::initIteration()
-{
-    for (unsigned long iBody = 0; iBody < this->getBodies().getN(); iBody++) {
-        this->accelerations[iBody].ax = 0.f;
-        this->accelerations[iBody].ay = 0.f;
-        this->accelerations[iBody].az = 0.f;
-    }
-}
-
 
 void SimulationNBodySIMD::initIteration()
 {
-    for (unsigned long iBody = 0; iBody < this->getBodies().getN(); iBody += this->N) {
-        this->accelerations[iBody].ax = 0.f;
-        this->accelerations[iBody].ay = 0.f;
-        this->accelerations[iBody].az = 0.f;
-    }
+    std::fill(this->vAccelerations.ax.begin(), this->vAccelerations.ax.begin(), 0.f);
+    
+    std::fill(this->vAccelerations.ay.begin(), this->vAccelerations.ay.begin(), 0.f);
+    std::fill(this->vAccelerations.az.begin(), this->vAccelerations.az.begin(), 0.f);
 }
-
-void SimulationNBodyNaive::computeBodiesAcceleration()
-{
-    const std::vector<dataAoS_t<float>> &d = this->getBodies().getDataAoS();
-
-    // flops = n² * 20
-    for (unsigned long iBody = 0; iBody < this->getBodies().getN(); iBody++) {
-        // flops = n * 20
-        for (unsigned long jBody = 0; jBody < this->getBodies().getN(); jBody++) {
-            const float rijx = d[jBody].qx - d[iBody].qx; // 1 flop
-            const float rijy = d[jBody].qy - d[iBody].qy; // 1 flop
-            const float rijz = d[jBody].qz - d[iBody].qz; // 1 flop
-
-            // compute the || rij ||² distance between body i and body j
-            const float rijSquared = std::pow(rijx, 2) + std::pow(rijy, 2) + std::pow(rijz, 2); // 5 flops
-            // compute e²
-            const float softSquared = std::pow(this->soft, 2); // 1 flops
-            // compute the acceleration value between body i and body j: || ai || = G.mj / (|| rij ||² + e²)^{3/2}
-            const float ai = this->G * d[jBody].m / std::pow(rijSquared + softSquared, 3.f / 2.f); // 5 flops
-
-            // add the acceleration value into the acceleration vector: ai += || ai ||.rij
-            this->accelerations[iBody].ax += ai * rijx; // 2 flops
-            this->accelerations[iBody].ay += ai * rijy; // 2 flops
-            this->accelerations[iBody].az += ai * rijz; // 2 flops
-        }
-    }
-}
-
 
 void SimulationNBodySIMD::computeBodiesAcceleration()
 {
     const dataSoA_t<float> &d = this->getBodies().getDataSoA();
 
     for (unsigned long iBody = 0; iBody < this->getBodies().getN(); iBody += this->floatN) {
-        	
-	    
+        
 	for(unsigned long jBody = 0; jBody < this->getBodies().getN(); jBody++){
+
+		//load and calculate distance for qx	
+		mipp::Reg<float> vqxi = d.qx[iBody];
+		mipp::Reg<float> vqxj = d.qx[jBody];
+		mipp::Reg<float> vqx = vqxj - vqxi;
+
+		//load and calculate distance for qy
+		mipp::Reg<float> vqyi = d.qy[iBody];
+		mipp::Reg<float> vqyj = d.qy[jBody];
+		mipp::Reg<float> vqy = vqyj - vqyi;
+		
+		//load and calculate distance for qz
+		mipp::Reg<float> vqzi = d.qz[iBody];
+		mipp::Reg<float> vqzj = d.qz[jBody];
+		mipp::Reg<float> vqz = vqzj - vqzi;
 	
+		mipp::Reg<float> vRijSquared = vqx*vqx + vqy*vqy + vqz*vqz;
+
+		mipp::Reg<float> vSoftSquared = this->soft * this->soft;
+	
+
+		// (1/sqrt(vRijSquared + vSoftSquared))^(3) = (vRijSquared + vSoftSquared)^(3/2)
+		mipp::Reg<float> rsqrt_cubed = rsqrt(vRijSquared + vSoftSquared);
+		rsqrt_cubed = rsqrt_cubed * rsqrt_cubed * rsqrt_cubed;
+
+		mipp::Reg<float> vAi = this->G + d.m[jBody];
+	       	vAi = vAi / rsqrt_cubed;	
+	
+		// this->vAccelerations.ax[iBody].store(vAi * vqx);
+		// this->vAccelerations.ay[iBody].store(vAi * vqy);
+		// this->vAccelerations.az[iBody].store(vAi * vqz);
+	
+		std::vector<float> tmp;
+		mipp::Reg<float> acceltemp = vAi*vqx;
+
+		acceltemp.store(&tmp);
+		this->vAccelerations.ax[iBody] += acceltemp;
+	
+		
+		acceltemp = vAi*vqy;
+
+		acceltemp.store(&tmp);
+		this->vAccelerations.ay[iBody] += acceltemp;
+
+			
+		acceltemp = vAi*vqz;
+
+		acceltemp.store(&tmp);
+		this->vAccelerations.az[iBody] += acceltemp;
 	}
     
     }
 }
 
-void SimulationNBodyNaive::computeOneIteration()
+void SimulationNBodySIMD::computeOneIteration()
 {
     this->initIteration();
     this->computeBodiesAcceleration();
     // time integration
-    this->bodies.updatePositionsAndVelocities(this->accelerations, this->dt);
+    this->bodies.updatePositionsAndVelocities(this->vAccelerations, this->dt);
 }
