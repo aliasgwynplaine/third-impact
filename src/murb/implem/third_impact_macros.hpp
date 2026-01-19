d04eb0f5c0aec28427daae0ecb685b9744830b8a
gwynplaine <mnleonr@uni.pe>
2026-01-19 22:21:24 +0100

    macro redef.

--- src/murb/implem/third_impact_macros.hpp
+++ src/murb/implem/third_impact_macros.hpp
@@ -1,7 +1,32 @@
 #ifndef THIRD_IMPACT_MACROS_H
 #define THIRD_IMPACT_MACROS_H
 
-#define SQUARE(X) ((X)*(X))
-#define POW3(X) ((X)*(X)*(X))
+#include <xmmintrin.h>
+
+#define SQUARE(x) ({          \
+    __typeof__(x) _x = (x);   \
+    _x * _x;                  \
+})
+
+#define POW3(x) ({           \
+    __typeof__(x) _x = (x);  \
+    _x * _x * _x;            \
+})
+
+#define FAST_POW(a, b) ({   \
+  union {                   \
+    double d;               \
+    int x[2];               \
+  } u = { a };              \
+  u.x[1] = (int)(b * (u.x[1] - 1072632447) + 1072632447); \
+  u.x[0] = 0;               \
+  u.d;                      \
+})
+
+
+#define FAST_RSQRT(x) ({ \
+    float ret; \
+    ret = _mm_cvtss_f32(_mm_rsqrt_ss(_mm_set_ss(x))); \
+    ret; })
 
 #endif
