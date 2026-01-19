95ba2e0a78e25748d2ae95cc2a27558be1cc546b
gwynplaine <mnleonr@uni.pe>
2026-01-19 22:34:20 +0100

    :P

--- src/murb/implem/third_impact_macros.hpp
+++ src/murb/implem/third_impact_macros.hpp
@@ -13,20 +13,18 @@
     _x * _x * _x;            \
 })
 
+// this can be fast but not so accurate uu
 #define FAST_POW(a, b) ({   \
-  union {                   \
-    double d;               \
-    int x[2];               \
-  } u = { a };              \
-  u.x[1] = (int)(b * (u.x[1] - 1072632447) + 1072632447); \
-  u.x[0] = 0;               \
-  u.d;                      \
+    union {                   \
+        double d;               \
+        int x[2];               \
+    } u = { a };              \
+    u.x[1] = (int)(b * (u.x[1] - 1072632447) + 1072632447); \
+    u.x[0] = 0;               \
+    u.d;                      \
 })
 
 
-#define FAST_RSQRT(x) ({ \
-    float ret; \
-    ret = _mm_cvtss_f32(_mm_rsqrt_ss(_mm_set_ss(x))); \
-    ret; })
+#define FAST_RSQRT(x) _mm_cvtss_f32(_mm_rsqrt_ss(_mm_set_ss(x)))
 
 #endif
