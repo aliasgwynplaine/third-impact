c435f642444ce37ce5fec2cc99c315616581c58c
lailabennani <laila.lahkim.bennani03@gmail.com>
2026-01-22 15:43:38 +0100

    it compiles :D

--- src/murb/implem/SimulationNBodySIMD.cpp
+++ src/murb/implem/SimulationNBodySIMD.cpp
@@ -14,13 +14,14 @@ SimulationNBodySIMD::SimulationNBodySIMD(const unsigned long nBodies, const std:
     : SimulationNBodyInterface(nBodies, scheme, soft, randInit)
 {
     this->flopsPerIte = 20.f * (float)this->getBodies().getN() * (float)this->getBodies().getN();
+
+    //TODO : need to allocate the ax ay and az
 }
 
 void SimulationNBodySIMD::initIteration()
 {
-    std::fill(this->vAccelerations.ax.begin(), this->vAccelerations.ax.begin(), 0.f);
-    
-    std::fill(this->vAccelerations.ay.begin(), this->vAccelerations.ay.begin(), 0.f);
+    std::fill(this->vAccelerations.ax.begin(), this->vAccelerations.ax.end(), 0.f); 
+    std::fill(this->vAccelerations.ay.begin(), this->vAccelerations.ay.end(), 0.f);
     std::fill(this->vAccelerations.az.begin(), this->vAccelerations.az.begin(), 0.f);
 }
 
@@ -28,60 +29,60 @@ void SimulationNBodySIMD::computeBodiesAcceleration()
 {
     const dataSoA_t<float> &d = this->getBodies().getDataSoA();
 
+    mipp::Reg<float> vG = this->G;
+
+    mipp::Reg<float> vSoftSquared = this->soft * this->soft;
+	
+
+    //sum var    
+    float qx_sum = 0.f, qy_sum = 0.f, qz_sum = 0.f;
+
     for (unsigned long iBody = 0; iBody < this->getBodies().getN(); iBody += this->floatN) {
-        
+       
+	//load i positions 
+	mipp::Reg<float> vqxi = d.qx[iBody];
+	mipp::Reg<float> vqyi = d.qy[iBody];
+	mipp::Reg<float> vqzi = d.qz[iBody];
+	
 	for(unsigned long jBody = 0; jBody < this->getBodies().getN(); jBody++){
 
-		//load and calculate distance for qx	
-		mipp::Reg<float> vqxi = d.qx[iBody];
+		//load j positions and calculate distance	
 		mipp::Reg<float> vqxj = d.qx[jBody];
 		mipp::Reg<float> vqx = vqxj - vqxi;
 
-		//load and calculate distance for qy
-		mipp::Reg<float> vqyi = d.qy[iBody];
 		mipp::Reg<float> vqyj = d.qy[jBody];
 		mipp::Reg<float> vqy = vqyj - vqyi;
 		
-		//load and calculate distance for qz
-		mipp::Reg<float> vqzi = d.qz[iBody];
 		mipp::Reg<float> vqzj = d.qz[jBody];
 		mipp::Reg<float> vqz = vqzj - vqzi;
-	
+		
+		//load j mass
+		mipp::Reg<float> vmj = d.m[jBody];
+
+		
 		mipp::Reg<float> vRijSquared = vqx*vqx + vqy*vqy + vqz*vqz;
 
-		mipp::Reg<float> vSoftSquared = this->soft * this->soft;
-	
 
 		// (1/sqrt(vRijSquared + vSoftSquared))^(3) = (vRijSquared + vSoftSquared)^(3/2)
 		mipp::Reg<float> rsqrt_cubed = rsqrt(vRijSquared + vSoftSquared);
 		rsqrt_cubed = rsqrt_cubed * rsqrt_cubed * rsqrt_cubed;
 
-		mipp::Reg<float> vAi = this->G + d.m[jBody];
-	       	vAi = vAi / rsqrt_cubed;	
+		mipp::Reg<float> vAi = vG * vmj / rsqrt_cubed;
 	
-		// this->vAccelerations.ax[iBody].store(vAi * vqx);
-		// this->vAccelerations.ay[iBody].store(vAi * vqy);
-		// this->vAccelerations.az[iBody].store(vAi * vqz);
+		mipp::Reg<float> vAccx = vAi * vqx;
+		mipp::Reg<float> vAccy = vAi * vqy;
+		mipp::Reg<float> vAccz = vAi * vqz;
+
+		qx_sum = mipp::sum(vAccx);
+		qy_sum = mipp::sum(vAccy);
+		qz_sum = mipp::sum(vAccz);
 	
-		std::vector<float> tmp;
-		mipp::Reg<float> acceltemp = vAi*vqx;
-
-		acceltemp.store(&tmp);
-		this->vAccelerations.ax[iBody] += acceltemp;
-	
-		
-		acceltemp = vAi*vqy;
-
-		acceltemp.store(&tmp);
-		this->vAccelerations.ay[iBody] += acceltemp;
-
-			
-		acceltemp = vAi*vqz;
-
-		acceltemp.store(&tmp);
-		this->vAccelerations.az[iBody] += acceltemp;
 	}
-    
+   
+	this->vAccelerations.ax[iBody] = qx_sum;
+	this->vAccelerations.ay[iBody] = qy_sum;
+	this->vAccelerations.az[iBody] = qz_sum;
+
     }
 }
 
