17a426be7717418f8c45aeae635f1598a62b7d07
gwynplaine <mnleonr@uni.pe>
2026-01-25 22:31:28 +0100

    new parser

--- parser.sh
+++ parser.sh
@@ -1,14 +1,13 @@
-if [ $# -lt 1 ]; then
-        echo "!!!give me your focking file!!! usage : bash <output.out>"
-        exit -1
-fi
+for f in $(ls *.out); do
+    OUTPUT_FILE=$f-parsed.txt
+    info_line=$(cat $f | grep "Entire simulation took")
+    impl=$(cat $f | grep implementation | awk '{print $5}')
+    ITERATIONS=$(cat $f | grep iterations | awk '{print $7}')
+    n=$(cat $f | grep bodies | awk '{print $7}')
+    time=$(echo "$info_line" | awk '{print $4}')
+    fps=$(echo "$info_line" | awk '{print $6}' | tr -d '(')
+    gflops=$(echo "$info_line" | awk '{print $8}')
+    echo "$impl,$ITERATIONS,$n,$time,$fps,$gflops" >> $OUTPUT_FILE
+done
 
-f=$1
-OUTPUT_FILE=$f-parsed.txt
-info_line=$(cat $f | grep "Entire simulation took")
-impl=$(cat $f | grep implementation | awk '{print $5}')
-ITERATIONS=$(cat $f | grep iterations | awk '{print $7}')
-time=$(echo "$info_line" | awk '{print $4}')
-fps=$(echo "$info_line" | awk '{print $6}' | tr -d '(')
-gflops=$(echo "$info_line" | awk '{print $8}')
-echo "$impl,$ITERATIONS,$n,$time,$fps,$gflops" >> $OUTPUT_FILE
+cat *-parsed.txt > data.csv
